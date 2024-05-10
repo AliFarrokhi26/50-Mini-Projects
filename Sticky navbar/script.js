150bc895422b6c3393f8289a2c9aaa6daaa2a1ba
Ali Farrokhi <master.ali026@gmail.com>
2024-05-10 13:43:48 +0330

    Auto text added to hero title

--- Sticky navbar/script.js
+++ Sticky navbar/script.js
@@ -1,4 +1,21 @@
 const nav = document.querySelector('.nav')
+const herotitle= document.getElementById('heroH1')
+const heroText='Welcome To My Website'
+let index = 0
+liveHeroAnimation()
+
+
+function liveHeroAnimation(){
+herotitle.innerText=heroText.slice(0,index)
+index++
+
+if (index > heroText.length) {
+    index = 0
+}
+
+setTimeout(liveHeroAnimation,100);
+}
+
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
